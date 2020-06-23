09959616b8f18035547334b4d5bc3932f194abac
SergeyRiabov <ka4kaban@gmail.com>
2020-06-23 17:51:09 +0300

    dateHelper.js

--- src/components/Blocks/TopicBlock/TopicBlock.jsx
+++ src/components/Blocks/TopicBlock/TopicBlock.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import string from 'prop-types';
 import { withRouter } from 'react-router';
 import './topic-block.scss';
+import { formatDate } from '../../../helpers/dateHelper';
 
 
 class TopicBlockComponent extends React.Component {
@@ -10,16 +11,18 @@ class TopicBlockComponent extends React.Component {
     id: string,
     date: string,
   }
+
   onClick = () => {
     const { id, history } = this.props;
     history.push('/news/' + id);
   }
+
   render() {
     const { caption, date } = this.props;
     return (<div className="topic-block" onClick={this.onClick}>
       {caption}
       <div className="topic-block__date">
-        {date}
+        {formatDate(date)}
       </div>
     </div>)
   }
---
+++ src/helpers/dateHelper.js
@@ -0,0 +1,12 @@
+export const formatDate = function(dateString) {
+    let d = new Date(dateString);
+    let cur = new Date();
+    if (!d.getDate()) {
+      return dateString;
+    }
+    if (d.getDay() !== cur.getDay() || d.getMonth() !== cur.getMonth() || d.getYear() !== cur.getYear()) {
+        debugger
+      return d.getDay() + '-' + d.getMonth() + '-' + d.getFullYear();
+    }
+    return d.getHours() + ':' + d.getMinutes();
+  }
