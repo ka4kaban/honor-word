5377a91382e19e439f1134334de64e19120563b0
SergeyRiabov <ka4kaban@gmail.com>
2020-06-24 16:34:28 +0300

    box shadows

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -3,19 +3,32 @@ import './news-page.scss';
 import { connect } from 'react-redux';
 import { store } from '../../redux/configureStore';
 import { loadArticleByIdAction } from '../../redux/actions/actions';
+import { Header } from '../../components/Header/Header';
 import { selectArticle } from '../../redux/reducers/articlesReducer';
+import { Footer } from '../../components/Footer/Footer';
+import { NewsFeed } from '../../components/NewsFeed/NewsFeed';
 
 export class NewsPageComponent extends React.Component {
   componentDidMount() {
-    let {id} = this.props.match.params;
-    id = id.replace(':','');
+    let { id } = this.props.match.params;
+    id = id.replace(':', '');
     store.dispatch(loadArticleByIdAction(id))
   }
   render() {
-    const {article} = this.props;
+    const { article } = this.props;
     return (
       <div className="news-page">
-        {article && article.content}
+        <Header />
+        <div className="main-page__content-container">
+          <div className="main-page__innerView-container">
+            {article && article.content}
+          </div>
+          <div className="main-page__news-feed-container">
+            <NewsFeed />
+          </div>
+        </div>
+        <Footer />
+
       </div>
     )
   }
